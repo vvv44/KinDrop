5017e9d47da6515c915c7442b5a1a66242d6afe8
Victor Vargas <vevargascor@miners.utep.edu>
2020-03-22 16:05:56 -0600

    added php that returns all values on database

---
+++ php/searchLists.php
@@ -0,0 +1,19 @@
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:kindrop.database.windows.net,1433; Database = kindrop Wishlists", "kindropadmin", "vsilva13/09");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sth = $conn->prepare('SELECT * FROM wishlists');
+    $sth->execute();
+    $rows = $sth->fetchAll(PDO::FETCH_CLASS);
+    $rowsToReturn;
+    foreach ($rows as $row) {
+        $rowsToReturn[] = $row;
+    }
+    echo json_encode($rowsToReturn);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+?>
